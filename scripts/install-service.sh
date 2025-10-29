49eb76ebdc290624c8fc89ed6ec6718a8a0f358d
zhangxinhui02 <i@miku.team>
2025-10-30 02:19:40 +0800

    feat: 添加Linux安装脚本的命令检查

--- scripts/install-service.sh
+++ scripts/install-service.sh
@@ -3,6 +3,13 @@ set -e
 
 INTERVAL=${1:-"60"}  # 登录检查周期（秒），默认为60
 
+if command -v cqupt-internet >/dev/null 2>&1; then
+    echo "Command 'cqupt-internet' check OK."
+else
+    echo "Command 'cqupt-internet' required. Install the command first."
+    exit 1
+fi
+
 cd ..
 cp assets/cqupt-internet-autologin.service /etc/systemd/system
 sed -i "s|\${INTERVAL}|${INTERVAL}|g" /etc/systemd/system/cqupt-internet-autologin.service
